cad7ba63e4d9254fcbddd066f5766a7e3ab915d2
Alex.Eloy <Alex.Eloy@nielseniq.com>
2021-07-08 15:02:21 -0300

    First commit

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import "./style.css"
+
+
+function CardGame() {
+    return `
+        <article class="card-game">
+         <img src="./images/favicon.png" alt="Logo da Ebit">
+        </article>
+    `;
+}
+
+export default CardGame;
