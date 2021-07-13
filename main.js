d6a026ac4bf5589d693924be0a03d61dfe0a8edf
Alex.Eloy <Alex.Eloy@nielseniq.com>
2021-07-13 16:03:28 -0300

    Add CardFrontBack in BordGame

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const $root = document.querySelector("#root");
 $root.insertAdjacentHTML(
     "beforeend", 
     `
-    ${ScoreBoard()}    
-    ${BoardGame(6)}
+    ${ScoreBoard()}  
+    ${BoardGame(2)}
     `
     );
